827f28d6a25a21b84f8f47ec958cf65650b94580
Itamar Medrano <martinezitamar777@gmail.com>
2024-03-01 11:54:30 -0600

    cambio

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@ let nombre = document.getElementById("nombre").value
 let dui = document.getElementById("dui").value
 let edad = document.getElementById("edad").value
 let telefono = document.getElementById("telefono").value
-let 
+let voleto = document.getElementById("voleto").value
 
-validarEdad{
-    if (condition) {
-        
+ function validarEdad(){
+    let edad = document.getElementById("edad").value
+    if (edad < 17) {
+        alert`Losiento Nose Pueden hacer ventas a menores de edad`
     }
 }
