9f2f710067dc580fd2ba0fe85206bdd4dad5a30e
Itamar Medrano <martinezitamar777@gmail.com>
2024-03-01 11:31:18 -0600

    u

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let numerovoleto = document.getElementById("numerovoleto");
+let precio = document.getElementById("precio");
+let estado = document.getElementById("estado");
+
+let nombre = document.getElementById("nombre").value
+let dui = document.getElementById("dui").value
+let edad = document.getElementById("edad").value
+let telefono = document.getElementById("telefono").value
+let 
+
+validarEdad{
+    if (condition) {
+        
+    }
+}
